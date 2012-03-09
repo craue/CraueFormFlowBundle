136c283bdde6907bcd9258842312d89b76aec075
Toni Uebernickel <tuebernickel@gmail.com>
2012-03-09 15:11:55 +0100

    add StorageInterface to exchange step data storage

---
+++ Storage/StorageInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Craue\FormFlowBundle\Storage;
+
+/**
+ * @author Toni Uebernickel <tuebernickel@gmail.com>
+ * @copyright 2011-2012 Christian Raue
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+interface StorageInterface
+{
+	/**
+	 * Store the given value under the provided key.
+	 *
+	 * @param string $key
+	 * @param mixed $value
+	 */
+	function set($key, $value);
+
+	/**
+	 * Retrieve the data stored under the given key.
+	 *
+	 * @param string $key
+	 *
+	 * @return mixed
+	 */
+	function get($key);
+
+	/**
+	 * Delete the stored data of the given key.
+	 *
+	 * @param string $key
+	 */
+	function remove($key);
+}
