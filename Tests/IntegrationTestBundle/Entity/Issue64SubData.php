1944cbbc6906de6ea7dbb3485ed4a9d852a2cb71
Christian Raue <christian.raue@gmail.com>
2013-06-23 10:59:46 +0200

    added a test for #64

---
+++ Tests/IntegrationTestBundle/Entity/Issue64SubData.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Craue\FormFlowBundle\Tests\IntegrationTestBundle\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @author Christian Raue <christian.raue@gmail.com>
+ * @copyright 2011-2013 Christian Raue
+ * @license http://opensource.org/licenses/mit-license.php MIT License
+ */
+class Issue64SubData {
+
+	/**
+	 * @var string
+	 * @Assert\NotBlank(groups={"flow_issue64_step1"})
+	 */
+	public $prop1;
+
+	/**
+	 * @var string
+	 * @Assert\NotBlank(groups={"flow_issue64_step2"})
+	 */
+	public $prop2;
+
+}
---
+++ Tests/IntegrationTestBundle/Form/Issue64Flow.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Craue\FormFlowBundle\Tests\IntegrationTestBundle\Form;
+
+use Craue\FormFlowBundle\Form\FormFlow;
+use Craue\FormFlowBundle\Form\FormFlowInterface;
+
+/**
+ * @author Christian Raue <christian.raue@gmail.com>
+ * @copyright 2011-2013 Christian Raue
+ * @license http://opensource.org/licenses/mit-license.php MIT License
+ */
+class Issue64Flow extends FormFlow {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getName() {
+		return 'issue64';
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	protected function loadStepsConfig() {
+		return array(
+			array(
+				'label' => 'step1',
+				'type' => 'issue64',
+			),
+			array(
+				'label' => 'step2',
+				'type' => 'issue64',
+			),
+			array(
+				'label' => 'step3',
+				'type' => 'issue64',
+			),
+			array(
+				'label' => 'step4',
+				'type' => 'issue64',
+			),
+		);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getFormOptions($step, array $options = array()) {
+		$options = parent::getFormOptions($step, $options);
+
+		$options['cascade_validation'] = true;
+		$options['flowStep'] = $step;
+
+		return $options;
+	}
+
+}
