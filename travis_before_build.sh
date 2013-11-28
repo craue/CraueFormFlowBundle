55182ed18b1810aa4b7d88f35a64e015c2be1974
Christian Raue <christian.raue@gmail.com>
2013-11-28 16:19:35 +0100

    let Travis run tests with Symfony 2.4.x stable and 2.5.x dev; adapted test setup to desynchronized version numbering of SensioFrameworkExtraBundle

--- travis_before_build.sh
+++ travis_before_build.sh
@@ -4,8 +4,10 @@ wget http://getcomposer.org/composer.phar
 
 php composer.phar config -g preferred-install source
 
-sed -i -e "s/\"minimum-stability\": \"stable\"/\"minimum-stability\": \"${MIN_STABILITY}\"/" composer.json
+if [ -n "${MIN_STABILITY:-}" ]; then
+	sed -i -e "s/\"minimum-stability\": \"stable\"/\"minimum-stability\": \"${MIN_STABILITY}\"/" composer.json
+fi
 
-php composer.phar --no-interaction --no-update require symfony/framework-bundle:${SYMFONY_VERSION} symfony/form:${SYMFONY_VERSION}
-php composer.phar --no-interaction --no-update require --dev sensio/framework-extra-bundle:${SYMFONY_VERSION} symfony/symfony:${SYMFONY_VERSION}
+php composer.phar --no-interaction require --no-update symfony/framework-bundle:${SYMFONY_VERSION} symfony/form:${SYMFONY_VERSION}
+php composer.phar --no-interaction require --no-update --dev sensio/framework-extra-bundle:${SENSIO_FRAMEWORK_EXTRA_BUNDLE_VERSION:-${SYMFONY_VERSION}} symfony/symfony:${SYMFONY_VERSION}
 php composer.phar --no-interaction update
