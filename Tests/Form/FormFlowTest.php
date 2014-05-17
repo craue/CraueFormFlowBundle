6c1befc40a9c629671d30c2501fc21cc1760049e
Christian Raue <christian.raue@gmail.com>
2014-05-17 15:22:20 +0200

    tests for code relying on GET and POST, preparation for PUT support

--- Tests/Form/FormFlowTest.php
+++ Tests/Form/FormFlowTest.php
@@ -5,6 +5,9 @@ namespace Craue\FormFlowBundle\Tests\Form;
 use Craue\FormFlowBundle\Event\GetStepsEvent;
 use Craue\FormFlowBundle\Form\FormFlowEvents;
 use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\Form\Forms;
+use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @group unit
@@ -95,6 +98,99 @@ class FormFlowTest extends \PHPUnit_Framework_TestCase {
 		$options = $flow->getFormOptions(1);
 
 		$this->assertEquals('targetUrl', $options['action']);
+	}
+
+	/**
+	 * Ensure that the requested step number is correctly determined.
+	 *
+	 * @dataProvider dataGetRequestedStepNumber
+	 *
+	 * @param string $httpMethod The HTTP method.
+	 * @param array $parameters Parameters for the query/request.
+	 * @param boolean $dsnEnabled If dynamic step navigation is enabled.
+	 * @param integer $expectedStepNumber The expected step number being requested.
+	 */
+	public function testGetRequestedStepNumber($httpMethod, $parameters, $dsnEnabled, $expectedStepNumber) {
+		$flow = $this->getMock('\Craue\FormFlowBundle\Form\FormFlow', array('getName', 'getRequest'));
+
+		if ($dsnEnabled) {
+			$flow->setAllowDynamicStepNavigation(true);
+		}
+
+		$flow
+			->expects($this->any())
+			->method('getName')
+			->will($this->returnValue('createTopic'))
+		;
+
+		$flow
+			->expects($this->once())
+			->method('getRequest')
+			->will($this->returnValue(Request::create('', $httpMethod, $parameters)))
+		;
+
+		$method = new \ReflectionMethod($flow, 'getRequestedStepNumber');
+		$method->setAccessible(true);
+
+		$this->assertSame($expectedStepNumber, $method->invoke($flow));
+	}
+
+	public function dataGetRequestedStepNumber() {
+		return array(
+			array('GET', array(), false, 1),
+			array('GET', array(), true, 1),
+			array('GET', array('step' => 2), true, 2),
+			array('POST', array(), false, 1),
+			array('POST', array('flow_createTopic_step' => 2), false, 2),
+			array('PUT', array(), false, 1),
+// 			array('PUT', array('flow_createTopic_step' => 2), false, 2),
+		);
+	}
+
+	/**
+	 * Ensure that the form is correctly considered valid.
+	 *
+	 * @dataProvider dataIsValid
+	 *
+	 * @param string $httpMethod The HTTP method.
+	 * @param array $parameters Parameters for the query/request.
+	 * @param integer $expectedStepNumber The expected step number being requested.
+	 */
+	public function testIsValid($httpMethod, $parameters, $expectedValid) {
+		$flow = $this->getMock('\Craue\FormFlowBundle\Form\FormFlow', array('getName', 'getRequest'));
+
+		$flow->setRevalidatePreviousSteps(false);
+
+		$flow
+			->expects($this->any())
+			->method('getName')
+			->will($this->returnValue('createTopic'))
+		;
+
+		$flow
+			->expects($this->any())
+			->method('getRequest')
+			->will($this->returnValue(Request::create('', $httpMethod, $parameters)))
+		;
+
+		$dispatcher = $this->getMock('Symfony\Component\EventDispatcher\EventDispatcherInterface');
+		$factory = Forms::createFormFactoryBuilder()->getFormFactory();
+		$formBuilder = new FormBuilder(null, null, $dispatcher, $factory);
+		$form = $formBuilder->getForm();
+
+		$this->assertSame($expectedValid, $flow->isValid($form));
+	}
+
+	public function dataIsValid() {
+		return array(
+			array('GET', array(), false),
+			array('POST', array(), true),
+			array('POST', array('flow_createTopic_transition' => 'back'), false),
+			array('POST', array('flow_createTopic_transition' => 'reset'), false),
+// 			array('PUT', array(), true),
+			array('PUT', array('flow_createTopic_transition' => 'back'), false),
+			array('PUT', array('flow_createTopic_transition' => 'reset'), false),
+		);
 	}
 
 }
