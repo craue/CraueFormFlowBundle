0b2dd452177e35686e2ae385d61a40f78c6ff3e1
Christian Raue <christian.raue@gmail.com>
2014-06-26 23:56:39 +0200

    finer grained Composer requirements

--- travis_before_build.sh
+++ travis_before_build.sh
@@ -8,6 +8,6 @@ if [ -n "${MIN_STABILITY:-}" ]; then
 	sed -i -e "s/\"minimum-stability\": \"stable\"/\"minimum-stability\": \"${MIN_STABILITY}\"/" composer.json
 fi
 
-php composer.phar --no-interaction require --no-update symfony/framework-bundle:${SYMFONY_VERSION} symfony/form:${SYMFONY_VERSION}
+php composer.phar --no-interaction require --no-update symfony/form:${SYMFONY_VERSION} symfony/http-kernel:${SYMFONY_VERSION} symfony/translation:${SYMFONY_VERSION} symfony/yaml:${SYMFONY_VERSION}
 php composer.phar --no-interaction require --no-update --dev sensio/framework-extra-bundle:${SENSIO_FRAMEWORK_EXTRA_BUNDLE_VERSION:-${SYMFONY_VERSION}} symfony/symfony:${SYMFONY_VERSION}
 php composer.phar --no-interaction update
