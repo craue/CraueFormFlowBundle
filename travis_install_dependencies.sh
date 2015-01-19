6e4691bcb8a0a79623df9edfe6ad60c642a6f8bf
Christian Raue <christian.raue@gmail.com>
2015-01-19 12:27:47 +0100

    backported test environment config from master

---
+++ travis_install_dependencies.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+composer config -g preferred-install source
+
+if [ -n "${MIN_STABILITY:-}" ]; then
+	sed -i -e "s/\"minimum-stability\": \"stable\"/\"minimum-stability\": \"${MIN_STABILITY}\"/" composer.json
+fi
+
+composer --no-interaction require --no-update satooshi/php-coveralls:~0.6@stable
+composer --no-interaction require --no-update symfony/framework-bundle:${SYMFONY_VERSION} symfony/form:${SYMFONY_VERSION}
+composer --no-interaction require --no-update --dev sensio/framework-extra-bundle:${SENSIO_FRAMEWORK_EXTRA_BUNDLE_VERSION:-${SYMFONY_VERSION}} symfony/symfony:${SYMFONY_VERSION}
+composer --no-interaction update
