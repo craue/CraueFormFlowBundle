a3d677f687bb1ada8eb803438d9eaaf3089ed4a4
Christian Raue <christian.raue@gmail.com>
2015-09-30 11:42:02 +0200

    use DI's `Extension` class directly

--- DependencyInjection/CraueFormFlowExtension.php
+++ DependencyInjection/CraueFormFlowExtension.php
@@ -4,8 +4,8 @@ namespace Craue\FormFlowBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * Registration of the extension via DI.
