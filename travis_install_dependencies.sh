320000296169c0a34f7857cf69633e88ddecbbe4
Christian Raue <christian.raue@gmail.com>
2016-03-03 08:29:07 +0100

    replaced double by single quotes where possible

--- travis_install_dependencies.sh
+++ travis_install_dependencies.sh
@@ -5,7 +5,7 @@ set -euv
 export COMPOSER_NO_INTERACTION=1
 composer self-update
 
-if [ "${TRAVIS_PHP_VERSION}" = "5.3.3" ]; then
+if [ "${TRAVIS_PHP_VERSION}" = '5.3.3' ]; then
 	composer config -g disable-tls true
 fi
 
@@ -19,8 +19,8 @@ if [ -n "${SYMFONY_VERSION:-}" ]; then
 	composer require --no-update --dev symfony/symfony:"${SYMFONY_VERSION}"
 fi
 
-if [ "${USE_DEPS:-}" = "lowest" ]; then
-	COMPOSER_UPDATE_ARGS="--prefer-lowest"
+if [ "${USE_DEPS:-}" = 'lowest' ]; then
+	COMPOSER_UPDATE_ARGS='--prefer-lowest'
 fi
 
 composer update ${COMPOSER_UPDATE_ARGS:-}
