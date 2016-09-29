52fd639987efe4308df20041c838c2a5832f871d
Christian Raue <christian.raue@gmail.com>
2016-09-29 14:40:27 +0200

    run one Travis job with unmodified dependencies

--- .travis_install_dependencies.sh
+++ .travis_install_dependencies.sh
@@ -11,18 +11,23 @@ fi
 
 composer self-update
 
-if [ -n "${MIN_STABILITY:-}" ]; then
-	sed -i -e "s/\"minimum-stability\": \"stable\"/\"minimum-stability\": \"${MIN_STABILITY}\"/" composer.json
-fi
+case "${DEPS:-}" in
+	'lowest')
+		COMPOSER_UPDATE_ARGS='--prefer-lowest'
+		;;
+	'unmodified')
+		# don't modify dependencies, install them as defined
+		;;
+	*)
+		if [ -n "${MIN_STABILITY:-}" ]; then
+			sed -i -e "s/\"minimum-stability\": \"stable\"/\"minimum-stability\": \"${MIN_STABILITY}\"/" composer.json
+		fi
 
-composer remove --no-update symfony/framework-bundle symfony/form
+		composer remove --no-update symfony/framework-bundle symfony/form
 
-if [ -n "${SYMFONY_VERSION:-}" ]; then
-	composer require --no-update --dev symfony/symfony:"${SYMFONY_VERSION}"
-fi
-
-if [ "${USE_DEPS:-}" = 'lowest' ]; then
-	COMPOSER_UPDATE_ARGS='--prefer-lowest'
-fi
+		if [ -n "${SYMFONY_VERSION:-}" ]; then
+			composer require --no-update --dev symfony/symfony:"${SYMFONY_VERSION}"
+		fi
+esac
 
 composer update ${COMPOSER_UPDATE_ARGS:-}
