84ce0dbe6e11c78d9ac1c697f28d95aae67735c3
Christian Raue <christian.raue@gmail.com>
2020-06-24 18:40:52 +0200

    stick to PHPStan 0.12.25 for now

--- .travis_install_dependencies.sh
+++ .travis_install_dependencies.sh
@@ -23,7 +23,7 @@ case "${DEPS:-}" in
 esac
 
 if [ -n "${WITH_STATIC_ANALYSIS:-}" ]; then
-	composer require --no-update --dev "phpstan/phpstan:^0.12"
+	composer require --no-update --dev "phpstan/phpstan:0.12.25"
 fi
 
 # TODO remove as soon as Symfony >= 4.2 is required
