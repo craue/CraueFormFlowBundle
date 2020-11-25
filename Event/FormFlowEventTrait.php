650dab96c5241e45bf7832d66ada3988b45376e2
Piotr Antosik <piotr@ant.qa>
2020-11-25 11:07:18 +0100

    Fix opcache preloading (#368)
    Fix opcache preloading

---
+++ Event/FormFlowEventTrait.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Craue\FormFlowBundle\Event;
+
+use Craue\FormFlowBundle\Form\FormFlowInterface;
+
+/**
+ * @internal
+ * @author Christian Raue <christian.raue@gmail.com>
+ * @copyright 2011-2020 Christian Raue
+ * @license http://opensource.org/licenses/mit-license.php MIT License
+ */
+trait FormFlowEventTrait {
+
+    /**
+     * @var FormFlowInterface
+     */
+    protected $flow;
+
+    /**
+     * @param FormFlowInterface $flow
+     */
+    public function __construct(FormFlowInterface $flow) {
+        $this->flow = $flow;
+    }
+
+    /**
+     * @return FormFlowInterface
+     */
+    public function getFlow() {
+        return $this->flow;
+    }
+}
