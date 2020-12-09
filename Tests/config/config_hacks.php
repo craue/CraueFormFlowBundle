8bf3d106ac44da953308e874bae2830b80135634
Christian Raue <christian.raue@gmail.com>
2020-12-09 13:01:16 +0100

    explicitly enable DBAL to appease RegisterUidTypePass while running tests with Symfony 5.2.0

--- Tests/config/config_hacks.php
+++ Tests/config/config_hacks.php
@@ -10,3 +10,11 @@ if (Kernel::VERSION_ID >= 40300 && Kernel::VERSION_ID < 60000) {
 		],
 	]);
 }
+
+// TODO remove as soon as Symfony > 5.2.0 is required
+// explicitly enable DBAL to appease RegisterUidTypePass (only for Symfony 5.2.0), see https://github.com/symfony/symfony/issues/39400
+if (Kernel::VERSION_ID == 50200) {
+	$container->loadFromExtension('doctrine', [
+		'dbal' => [],
+	]);
+}
